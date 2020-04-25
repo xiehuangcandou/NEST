551b06a62894e3ef4678fd98f36412372ef431c5
NestCore <nestofficial@163.com>
2020-04-25 18:59:06 +0800

    Nest2.0

---
+++ Nest2.0/NEST_MiningSave.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.5.8;
+
+/**
+ * @title Ore pool
+ * @dev Storage and release nest
+ */
+contract NEST_MiningSave {
+    
+    IBMapping mappingContract;                      //  Mapping contract
+    ERC20 nestContract;                             //  Nest contract
+    
+    /**
+    * @dev Initialization method
+    * @param map Mapping contract address
+    */
+    constructor (address map) public {
+        mappingContract = IBMapping(address(map));              
+        nestContract = ERC20(address(mappingContract.checkAddress("nest")));            
+    }
+    
+    /**
+    * @dev Change mapping contract
+    * @param map Mapping contract address
+    */
+    function changeMapping(address map) public onlyOwner {
+        mappingContract = IBMapping(address(map));              
+        nestContract = ERC20(address(mappingContract.checkAddress("nest")));            
+    }
+    
+    /**
+    * @dev Transfer out nest
+    * @param amount Nest amount
+    * @param to target address
+    * @return Real quantity
+    */
+    function turnOut(uint256 amount, address to) public onlyMiningCalculation returns(uint256) {
+        uint256 leftNum = nestContract.balanceOf(address(this));
+        if (leftNum >= amount) {
+            nestContract.transfer(to, amount);
+            return amount;
+        } else {
+            return 0;
+        }
+    }
+    
+    modifier onlyOwner(){
+        require(mappingContract.checkOwners(msg.sender) == true);
+        _;
+    }
+
+    modifier onlyMiningCalculation(){
+        require(address(mappingContract.checkAddress("miningCalculation")) == msg.sender);
+        _;
+    }
+    
+}
+
+contract ERC20 {
+    function totalSupply() public view returns (uint supply);
+    function balanceOf( address who ) public view returns (uint value);
+    function allowance( address owner, address spender ) public view returns (uint _allowance);
+
+    function transfer( address to, uint256 value) external;
+    function transferFrom( address from, address to, uint value) public returns (bool ok);
+    function approve( address spender, uint value ) public returns (bool ok);
+
+    event Transfer( address indexed from, address indexed to, uint value);
+    event Approval( address indexed owner, address indexed spender, uint value);
+}
+
+
+contract IBMapping {
+	function checkAddress(string memory name) public view returns (address contractAddress);
+	function checkOwners(address man) public view returns (bool);
+}
